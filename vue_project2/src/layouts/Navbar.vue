43ea1e65fbf3bd7b53ccdc225a2355ef2e995c55
unknown <grabbany9258>
2023-02-12 19:22:01 +0600

    axios & templating & Routing

--- vue_project2/src/layouts/Navbar.vue
+++ vue_project2/src/layouts/Navbar.vue
@@ -3,16 +3,22 @@
     <div class="container-fluid">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
+          <router-link to="/" class="nav-link">Home</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <router-link to="/about" class="nav-link">About</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <router-link to="/contact" class="nav-link">Contact</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+          <router-link to="/news" class="nav-link">News</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/products" class="nav-link">Products</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/search" class="nav-link">Search</router-link>
         </li>
       </ul>
     </div>
--- vue_project2/src/main.js
+++ vue_project2/src/main.js
@@ -16,6 +16,30 @@
 // import './assets/main.css'
 
 // For templating
-import { createApp } from "vue";
-import App from "./App.vue";
-createApp(App).mount("#app");
+import { createApp } from "vue"; // CreateApp
+import App from "./App.vue"; //main component
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+import Contact from "./pages/Contact.vue";
+import News from "./pages/News.vue";
+import Products from "./pages/Products.vue";
+import Search from "./pages/Search.vue";
+// for routing node modules theke asle ./ lage na
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/contact", component: Contact },
+  { path: "/news", component: News },
+  { path: "/products", component: Products },
+  { path: "/search", component: Search },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  linkActiveClass: "active",
+});
+
+createApp(App).use(router).mount("#app");
